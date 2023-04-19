f66cb1d2c1f367650640e7bc98e978248c7fdf57
ArshiaKSabet <arshia.kolachaei@hotmail.com>
2023-04-19 20:25:33 +0200

    clickme updated

--- src/Extras/ClickMe.jsx
+++ src/Extras/ClickMe.jsx
@@ -11,16 +11,16 @@ function ClickMe() {
   return (
     <div>
       <button className="click" onClick={handleClick}>Click me!</button>
-      {isBoxOpen && <div className="box"><span><h2>Questions I remember I failed at the interview and how I should've answered them</h2>
+      {isBoxOpen && <div className="box"><span><h2>These two questions, was the kind of questions you knew the answer to as soon as you walked out through the door <span className="emoji"></span></h2>
       <p><span className="question">1. If you would like to add something (for example another field to every object in the json-data), what would you do?</span>
         </p>
-        <p className="wrong-answer">What I answered: "I would convert it to a JSON, but I'm not sure"
+        <p> <span className="wrong-answer">What I answered: "I would convert it to a JSON, but I'm not sure"</span><span className="emoji">😓😱</span>
           </p>
         <p><span className="correct-answer">What I should've answered: </span>Since the top-level of a JSON-text can be an array, I would've chosen to go with the Array.map method, which creates a new array with the new results, with each index modified as you wanted.
           </p>
         <p><span className="question">2. How would you add the spaces for the rendered children-components?</span>
         </p>
-        <p className="wrong-answer">What I answered: "I think there is an attribute called line-spacing.."🤯
+        <p ><span className="wrong-answer">What I answered: "I think there is an attribute called line-spacing.."</span><span className="emoji">🤯</span>
           </p>
         <p><span className="correct-answer">What I should've answered: </span> The nested elements such as divs(example), creates an indentation effect where each nested element is a part of the hierarchy.
           </p>
